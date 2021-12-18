432eb01117a87fa0e59411df711843a3eb807eaf
Evgeniy Kucherivaenko <draize26@gmail.com>
2021-12-18 21:35:08 +0700

    added API and realise his

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState,useEffect} from "react";
+
 import './Featured.css';
+import BTC from '../assets/btc-img.png';
+import {FiArrowUpRight} from 'react-icons/fi';
+import axios from "axios";
 
 const Featured = () => {
+
+    const [data,setData]=useState(null)
+
+    const URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false\n'
+
+    useEffect(()=> {
+        axios.get(URL).then((response)=> {
+            setData(response.data)
+        }). catch(error=> {
+            console.log(error)
+        })
+    }, [])
+
+
     return (
         <div className="featured">
             <div className="container">
@@ -13,6 +31,14 @@ const Featured = () => {
                 </div>
             </div>
             <div className="right">
+                <div className="top">
+                    <img src={BTC} alt="BTC img"/>
+                </div>
+                <div>
+                    <h5>Bitcoin</h5>
+                    <p>$49,000</p>
+                </div>
+                <span><FiArrowUpRight/>2.5%</span>
 
             </div>
         </div>
