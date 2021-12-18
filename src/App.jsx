fb61ea512219419c4b8f125d7c6d501ab35ea822
Evgeniy Kucherivaenko <draize26@gmail.com>
2021-12-18 20:43:26 +0700

    added Featured components and styled his

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from "./components/Hero";
+import Featured from "./components/Featured";
+
 const App = () => {
     return (
         <>
             <Navbar/>
             <Hero/>
+            <Featured/>
         </>
     );
 };
---
+++ src/components/Featured.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Featured.css';
+
+const Featured = () => {
+    return (
+        <div className="featured">
+            <div className="container">
+                {/*{LEFT}*/}
+                <div className="left">
+                    <h2>Explorer top Crypto's Like Bitcoin> Ethereum, and Dogecoin </h2>
+                <p>Sell al available assets: Cryptocurrencies and NFT'sy</p>
+                <button className="btn">See More Coins</button>
+                </div>
+            </div>
+            <div className="right">
+
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
