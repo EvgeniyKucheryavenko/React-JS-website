5df096c802dbbf17d47f606626f4d0fc50f1faee
Evgeniy Kucherivaenko <draize26@gmail.com>
2021-12-19 12:13:46 +0700

    added SignUp components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from "./components/Hero";
 import Featured from "./components/Featured";
+import Signup from "./components/Signup";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Navbar/>
             <Hero/>
             <Featured/>
+            <Signup/>
         </>
     );
 };
